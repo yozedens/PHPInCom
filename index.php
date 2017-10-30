4ccee7fb9d62cb8b82c5c3a9241c17904d94e917
yuzudeng <491254074@qq.com>
2017-10-30 17:43:27 +0800

    test

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     <head>
     </head>
     <body>
-
         <form action="index_ok.php" method="get" warp="hard">
             <input type="text" name="text" id="">
             <textarea name="textarea" id="" cols="30" rows="10">...显示默认文本内容</textarea>
