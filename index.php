89262fe5038a6802fe26ff1aa00d89f58d93cdd5
yuzudeng <491254074@qq.com>
2017-10-30 17:36:25 +0800

    text vscode push

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
         <form action="index_ok.php" method="get" warp="hard">
             <input type="text" name="text" id="">
-            <textarea name="textarea" id="" cols="30" rows="10">...默认文本内容</textarea>
+            <textarea name="textarea" id="" cols="30" rows="10">...显示默认文本内容</textarea>
             <input type="submit" name="submit" value="提交">
         </form>
     </body>
